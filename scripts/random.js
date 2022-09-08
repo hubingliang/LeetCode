6318a3047cee7b60e5013f7d6972dde38afc833a
bingliang.hu <bingliang.hu@advance.ai>
2022-09-08 11:15:12 +0800

    update script

---
+++ scripts/random.js
@@ -0,0 +1,7 @@
+const questionBank = [
+  "https://leetcode.cn/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/?envType=study-plan&id=lcof",
+  "https://leetcode.cn/problems/bao-han-minhan-shu-de-zhan-lcof/?envType=study-plan&id=lcof",
+  "https://leetcode.cn/problems/two-sum/",
+  "https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/",
+];
+console.log(questionBank[Math.floor(Math.random() * questionBank.length)]);
