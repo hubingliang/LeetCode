14629c34ad6f583d728eb7f7b25eb8b25a5a6aa1
bingliang.hu <bingliang.hu@advance.ai>
2022-09-09 14:32:56 +0800

    update question

--- scripts/random.js
+++ scripts/random.js
@@ -3,5 +3,6 @@ const questionBank = [
   "https://leetcode.cn/problems/bao-han-minhan-shu-de-zhan-lcof/?envType=study-plan&id=lcof",
   "https://leetcode.cn/problems/two-sum/",
   "https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/",
+  "https://leetcode.cn/problems/fan-zhuan-lian-biao-lcof/?plan=lcof&plan_progress=gbpnwlg",
 ];
 console.log(questionBank[Math.floor(Math.random() * questionBank.length)]);
