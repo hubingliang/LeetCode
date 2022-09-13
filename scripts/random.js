a9ab50689998ee4d640165287a91dc4676c32660
bingliang.hu <bingliang.hu@advance.ai>
2022-09-13 10:36:19 +0800

    update

--- scripts/random.js
+++ scripts/random.js
@@ -5,5 +5,6 @@ const questionBank = [
   "https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/",
   "https://leetcode.cn/problems/fan-zhuan-lian-biao-lcof/?plan=lcof&plan_progress=gbpnwlg",
   "https://leetcode.cn/problems/fu-za-lian-biao-de-fu-zhi-lcof/?envType=study-plan&id=lcof",
+  "https://leetcode.cn/problems/ti-huan-kong-ge-lcof/?envType=study-plan&id=lcof",
 ];
 console.log(questionBank[Math.floor(Math.random() * questionBank.length)]);
